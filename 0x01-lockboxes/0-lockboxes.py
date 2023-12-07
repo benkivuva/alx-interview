f0e8b3297193242442f1787690b8f60cca5a5d8a
benkivuva <benkivuvak5@gmail.com>
2023-12-07 01:27:24 -0800

    lockboxes solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module containing the canUnlockAll function to determine if all
+boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of list): A list of lists where each inner list represents
+                              a box and contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    myList = [0]
+    for i in myList:
+        for j in boxes[i]:
+            if j not in myList:
+                if j < n:
+                    myList.append(j)
+    if len(myList) == n:
+        return True
+    return False
